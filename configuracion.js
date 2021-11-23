9f685d9bbb25050ab92315b179ef656c88e3cab7
BuildTools <unconfigured@null.spigotmc.org>
2021-11-23 11:36:19 -0600

    update

---
+++ configuracion.js
@@ -0,0 +1,8 @@
+if(screen.width > 1270 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo1.css">')
+
+else if (screen.width < 1090 )
+
+document.write ('<link rel="stylesheet" href="posicionestodo2.css">')
+
