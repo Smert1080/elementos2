12cebcd9478a7ec3bf4fbcd77858b5d4babdf8f8
BuildTools <unconfigured@null.spigotmc.org>
2021-11-23 21:39:16 -0600

    update

--- configuracione.js
+++ configuracione.js
@@ -4,4 +4,4 @@ document.write ('<img src="encabezadoTN.jpg" width=1395px height=300px>')
 
 else if (screen.width < 1090 )
 
-document.write ('<img src="encabezadoTN.jpg" width=460px height=250px>')
+document.write ('<img src="encabezadoTN.jpg" width=440px height=250px>')
