a266cd63a43ee8740bc8fa3453e92c79445ef32c
BuildTools <unconfigured@null.spigotmc.org>
2021-11-30 00:32:37 -0600

    update

---
+++ configuracion-E.js
@@ -0,0 +1,7 @@
+if(screen.width > 1270 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo1E.css">')
+
+else if (screen.width < 1090 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo2E.css">')
---
+++ configuracioneE.js
@@ -0,0 +1,7 @@
+if(screen.width > 1270 )
+
+document.write ('<img src="encabezadoVRC.jpg" width=1395px height=300px>')
+
+else if (screen.width < 1090 )
+
+document.write ('<img src="encabezadoVRC.jpg" width=440px height=250px>')
