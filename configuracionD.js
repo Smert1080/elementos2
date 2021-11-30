25bc4925e6c7162de2c0f6077c9993215ed94972
BuildTools <unconfigured@null.spigotmc.org>
2021-11-29 23:25:07 -0600

    update

---
+++ configuracionD.js
@@ -0,0 +1,7 @@
+if(screen.width > 1270 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo1D.css">')
+
+else if (screen.width < 1090 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo2D.css">')
---
+++ configuracioneD.js
@@ -0,0 +1,7 @@
+if(screen.width > 1270 )
+
+document.write ('<img src="encabezadoVRF.jpg" width=1395px height=300px>')
+
+else if (screen.width < 1090 )
+
+document.write ('<img src="encabezadoVRF.jpg" width=440px height=250px>')
