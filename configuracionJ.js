eabff7e51c4c1642e2ce319be6b0796e313ef808
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 23:58:26 -0600

    update

---
+++ configuracionJ.js
@@ -0,0 +1,7 @@
+if(screen.width > 1270 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo1J.css">')
+
+else if (screen.width < 1090 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo2J.css">')
