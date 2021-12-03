3516f9cc02a98b6fb150cdccd7a7a52e3e162994
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 23:01:37 -0600

    update

---
+++ configuracionI.js
@@ -0,0 +1,7 @@
+if(screen.width > 1270 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo1I.css">')
+
+else if (screen.width < 1090 )
+
+document.write ('<link rel="stylesheet" href="pocisionestodo2I.css">')
